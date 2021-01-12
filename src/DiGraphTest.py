07f8a57bd19979dea57bde073a0c496a484e5eb7
Idanturjeman <idan_turjeman29@walla.co.il>
2021-01-12 13:38:33 +0200

    changes

---
+++ src/DiGraphTest.py
@@ -0,0 +1,70 @@
+import unittest
+##from DiGraph import DiGraph
+from Ariel_OOP_2020.Assignments.Ex3.src.DiGraph import DiGraph
+
+
+class MyTestCase(unittest.TestCase):
+
+    def test_adding(self):   # try adding nodes and edges check for correct value updates
+        graph = DiGraph()
+        for i in range(10):
+            self.assertTrue(graph.add_node(i))
+        for e in range(5):
+            self.assertTrue(graph.add_edge(e, e+1, e*10 + 1))
+
+        self.assertFalse(graph.add_edge(1, 1, 100))     # check if invalid addition is ignored
+        self.assertFalse(graph.add_edge(5, 80, 10))
+        self.assertFalse(graph.add_edge(8, 5, -5))
+        self.assertFalse(graph.add_edge(13, 15, 2))
+        self.assertFalse(graph.add_node(5))
+
+        self.assertEqual(graph.get_mc(), 15)
+        self.assertEqual(graph.e_size(), 5)
+        self.assertEqual(graph.v_size(), 10)
+
+    def test_removing(self):    # try removing nodes and edges then check for correct value updates
+        graph = DiGraph()
+        for i in range(10):
+            graph.add_node(i)
+        for e in range(5):
+            graph.add_edge(e, e + 1, e * 10 + 1)
+
+        self.assertFalse(graph.remove_edge(8, 9))         # check if invalid removal is ignored
+        self.assertFalse(graph.remove_edge(0, 100))
+
+        self.assertTrue(graph.remove_edge(4, 5))
+        self.assertEqual(graph.get_mc(), 16)
+        self.assertEqual(graph.e_size(), 4)
+
+        self.assertFalse(graph.remove_node(100))
+        self.assertTrue(graph.remove_node(2))
+
+        self.assertEqual(graph.get_mc(), 19)
+        self.assertEqual(graph.e_size(), 2)
+        self.assertEqual(graph.v_size(), 9)
+
+    def test_get_edges(self):     # check for correct edges' dict values
+        graph = DiGraph()
+        for i in range(6):
+            graph.add_node(i)
+        graph.add_edge(0, 1, 1)
+        graph.add_edge(2, 1, 3)
+        graph.add_edge(3, 1, 4)
+        graph.add_edge(1, 2, 3)
+        graph.add_edge(1, 5, 6)
+
+        # estimated values
+        check_out = {2: 3, 5: 6}
+        edges_out = graph.all_out_edges_of_node(1)
+        check_in = {0: 1, 2: 3, 3: 4}
+        edges_in = graph.all_in_edges_of_node(1)
+
+        # check every dictionary values
+        for e in edges_out:
+            self.assertEqual(check_out[e], edges_out[e])
+        for e in edges_in:
+            self.assertEqual(check_in[e], edges_in[e])
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ src/NodeTest.py
@@ -0,0 +1,69 @@
+import unittest
+from Ariel_OOP_2020.Assignments.Ex3.src.DiGraph import DiGraph
+
+
+class MyTestCase(unittest.TestCase):
+
+    # test addition and removal of edges
+    def test_edges(self):
+        node = DiGraph.Node(0, None)
+
+        self.assertFalse(node.add_edge(0, 100))   # check false addition
+        self.assertTrue(node.add_edge(1, 10))
+        self.assertTrue(node.add_edge(2, 20))
+        self.assertTrue(node.add_back_edge(2, 30))
+
+        # check if edge dictionary is correct
+        node_dict = node.get_edges()
+        self.assertIsNone(node_dict.get(0))
+        self.assertEqual(node_dict.get(1), 10)
+        self.assertEqual(node_dict.get(2), 20)
+
+        node_dict = node.get_back_edges()
+        self.assertEqual(node_dict.get(2), 30)
+
+        self.assertFalse(node.remove_edge(5))
+        self.assertFalse(node.remove_edge(0))
+        self.assertTrue(node.remove_edge(1))
+
+        # check new dictionary values
+        node_dict = node.get_edges()
+        self.assertIsNone(node_dict.get(5))
+        self.assertIsNone(node_dict.get(0))
+        self.assertIsNone(node_dict.get(1))
+        self.assertEqual(node_dict.get(2), 20)
+
+    def test_json(self):    # checks nodes string format
+        node1 = DiGraph.Node(1)
+        node2 = DiGraph.Node(2, (1, 2, 3))
+        node1.add_edge(2, 2)
+        node1.add_edge(3, 1)
+        node2.add_edge(4, 0)
+        node2.add_edge(5, 10)
+        pos = node2.get_pos()
+        str_node1 = "{\"id\": %d}" % node1.get_key()
+        str_node2 = "{\"pos\": \"%.16lf,%.16lf,%.16lf\", \"id\": %d}" % (pos[0], pos[1], pos[2], node2.get_key())
+        self.assertEqual(repr(node1), str_node1)
+        self.assertEqual(repr(node2), str_node2)
+
+
+
+
+
+    def test_path(self):    # test path function
+        node = DiGraph.Node(0)
+        p = [1, 2, 3]
+        node.set_path(p)
+        np = node.get_path()
+        # check the node's path values
+        for i in range(len(np)):
+            self.assertEqual(p[i], np[i])
+        node.set_path()
+        p = list()
+        np = node.get_path()
+        for i in range(len(p)):
+            self.assertEqual(p[i], np[i])
+
+
+if __name__ == '__main__':
+    unittest.main()
